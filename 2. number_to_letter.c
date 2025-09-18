2b61044cdf588bf3a40495a4d9670302d12b63fa
dksgudwn620 <vssssssssssg@gmail.com>
2025-09-18 12:50:04 +0900

    918

---
+++ 2. number_to_letter.c
@@ -0,0 +1,18 @@
+int main(void)
+{
+    int n;
+
+    printf("Á¤¼ö: ");
+    scanf("%d", &n);
+
+    if (n == 1)
+        puts("A");
+    else if (n == 2)
+        puts("B");
+    else if (n == 3)
+        puts("C");
+    else
+        puts("");
+
+    return 0;
+}
---
+++ test4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    printf("몇 단 삼각형입니까?: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n-1; j > i; j--)
+            printf(" ");
+        for (int j = 0; j < 2*i+1; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
