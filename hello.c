73aab0b06e6705e8032915675f2220e844b69f57
dksgudwn620 <vssssssssssg@gmail.com>
2025-09-18 10:45:18 +0900

    입력한 정수가 양수, 음수, 또는 0인지 판별하여 출력하는 간단한 C 프로그램

--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main() {
-    printf("He한글lo, World!\n");
-    return 0;
+int main(void)
+{
+    int n;
+
+    printf("정수: ");
+    scanf("%d", &n);
+
+    if (n > 0)
+        printf("양수입니다.\n");
+    else if (n < 0)
+        printf("음수입니다.\n");
+    else
+        printf("0입니다.\n");
 }
